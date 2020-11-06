14fefd3902a6469e9ab069d1cc69e084f6f52c01
Miklós András Danka <8069999+mdanka@users.noreply.github.com>
2020-11-06 20:26:08 +0100

    Updated usage to include mounting example

--- usage.py
+++ usage.py
@@ -2,27 +2,43 @@ import dash_facebook_login
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
+import dash_core_components as dcc
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
 
 app.layout = html.Div([
-    dash_facebook_login.DashFacebookLogin(
-        id='facebook-login',
-        appId='168590974451254',
-        version="v8.0",
-        cookie=True,
-        status=True,
-        autoLogoutLink=True,
-        scope="public_profile",
-        size="medium",
-        buttonType="login_with",
-        useContinueAs=True,
+    dcc.Checklist(
+        id='is-mounted-checkbox',
+        options=[{'label': 'Is button mounted?', 'value': 'is_mounted'}],
+        value=['is_mounted'],
     ),
-    html.Div(id='output')
+    html.Div(id='facebook-login-container'),
+    html.Div(id='facebook-login-output'),
 ])
 
 
-@app.callback(Output('output', 'children'), [Input('facebook-login', 'facebookLoginResponse')])
+
+@app.callback(Output('facebook-login-container', 'children'), [Input('is-mounted-checkbox', 'value')])
+def render_facebook_login_container(value):
+    if len(value) == 0:
+        return 'Button not mounted'
+    else:
+        return dash_facebook_login.DashFacebookLogin(
+            id='facebook-login',
+            appId='168590974451254',
+            version="v8.0",
+            cookie=True,
+            status=True,
+            xfbml=True,
+            autoLogoutLink=True,
+            scope="public_profile",
+            size="medium",
+            buttonType="login_with",
+            useContinueAs=True,
+        )
+
+
+@app.callback(Output('facebook-login-output', 'children'), [Input('facebook-login', 'facebookLoginResponse')])
 def display_output(value):
     return 'Response: {}'.format(value)
 
